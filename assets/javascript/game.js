be82ba2e8284b537727079dd0e873bbeadf3d315
kjige <chrisleongsc2@gmail.com>
2016-10-25 23:53:01 -0500

    select char not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,67 @@
-var obiwan = {
-		name: 'Obiwan Kenobi',
-		health: 120,
-		attack: 6,
-		counter: 20,
-		image: "assets/images/obi-wan.jpg"
-}
+var chars = [
+	obiwan = {
+			name: 'Obiwan Kenobi',
+			health: 120,
+			attack: 6,
+			counter: 20,
+			image: "assets/images/obi-wan.jpg"
+	},
 
-var luke = {
-		name: 'Luke Skywalker',
-		health: 100,
-		attack: 6,
-		counter: 10,
-		image: "assets/images/luke-skywalker.jpg"
-}
+	luke = {
+			name: 'Luke Skywalker',
+			health: 100,
+			attack: 6,
+			counter: 10,
+			image: "assets/images/luke-skywalker.jpg"
+	},
 
-var sidious = {
-		name: 'Darth Sidious',
-		health: 140,
-		attack: 6,
-		counter: 30,
-		image: "assets/images/darth-sidious.png"
-}
+	sidious = {
+			name: 'Darth Sidious',
+			health: 140,
+			attack: 6,
+			counter: 30,
+			image: "assets/images/darth-sidious.png"
+	},
 
-var maul = {
-		name: 'Darth Maul',
-		health: 160,
-		attack: 6,
-		counter: 40,
-		image: "assets/images/darth-maul.jpg"
-}
+	maul = {
+			name: 'Darth Maul',
+			health: 160,
+			attack: 6,
+			counter: 40,
+			image: "assets/images/darth-maul.jpg"
+	}
+]
 
-function createObiwan() {
-	var a = $('<p></p>').text(obiwan.name);
-	$(this).append(a);
-	var b = $('<img></img>');
+//loop to create chars
+for (var i = 0; i < chars.length; i++) {
+	var button = $('<button>');
+	button.attr({
+		'data-name': chars[i].name,
+		'data-health': chars[i].health,
+		'data-attack': chars[i].attack,
+		'data-counter': chars[i].counter,
+		'data-image': chars[i].image
+	});
+	var a = $('<p>').text(chars[i].name);
+	var b = $('<img>');
 	b.attr({
-		'src': obiwan.image,
-		'height': '100px',
-		})
-	$(this).append(b);
-	var c = $('<p></p>').text(obiwan.health);
-	$(this).append(c);
+	'src': chars[i].image,
+	'height': '100px'
+	});
+	var c = $('<p>').text(chars[i].health);
+	button.append(a,b,c);
+	$('.options').append(button);
 }
 
-function createLuke() {
-	var a = $('<p></p>').text(luke.name);
-	$(this).append(a);
-	var b = $('<img></img>');
-	b.attr({
-		'src': luke.image,
-		'height': '100px',
-		})
-	$(this).append(b);
-	var c = $('<p></p>').text(luke.health);
-	$(this).append(c);
-}
-
-function createSidious() {
-	var a = $('<p></p>').text(sidious.name);
-	$(this).append(a);
-	var b = $('<img></img>');
-	b.attr({
-		'src': sidious.image,
-		'height': '100px',
-		})
-	$(this).append(b);
-	var c = $('<p></p>').text(sidious.health);
-	$(this).append(c);
-}
-
-function createMaul() {
-	var a = $('<p></p>').text(maul.name);
-	$(this).append(a);
-	var b = $('<img></img>');
-	b.attr({
-		'src': maul.image,
-		'height': '100px',
-		})
-	$(this).append(b);
-	var c = $('<p></p>').text(maul.health);
-	$(this).append(c);
-}
-
-$('.chooseObiwan').createObiwan();
-$('.chooseLuke').createLuke();
-$('.chooseSidious').createSidious();
-$('.chooseMaul').createMaul();
+$('.options').click(function() {
+	// $('.options').hide();
+	var div = $('<div>');
+	var a = $('<p>').text($(this).data('name'));
+	var b = $('<img>').attr({
+		'src': $(this).data('image'),
+		'height': '100px'
+	});
+	var c = $('<p>').text($(this).data('health'));
+	div.append(a,b,c);
+	$('.selected').append(div);
+})
